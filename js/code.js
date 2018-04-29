9ca2f88e20796801120c6ae6f3e5e2792795e866
mallikancherla <37102689+mallikancherla@users.noreply.github.com>
2018-04-29 18:34:53 +0530

    upload

---
+++ js/code.js
@@ -0,0 +1,50 @@
+
+ var au = document.getElementById("aaud");
+ var aq=document.getElementById("a");
+ aq.onmousedown=function() {ac()};
+ function ac(){
+ au.currentTime = 0;
+ au.play();
+  }
+var bu = document.getElementById("baud");
+var bq=document.getElementById("b");
+bq.onmousedown=function() {bc()};
+function bc(){
+    bu.currentTime = 0;
+    bu.play();
+}
+var cu = document.getElementById("caud");
+var cq=document.getElementById("c");
+cq.onmousedown=function() {cc()};
+function cc(){
+cu.currentTime = 0;
+cu.play();
+ }
+var du = document.getElementById("daud");
+var dq=document.getElementById("d");
+dq.onmousedown=function() {dc()};
+function dc(){
+    du.currentTime = 0;
+    du.play();
+}
+var eu = document.getElementById("eaud");
+var eq=document.getElementById("e");
+eq.onmousedown=function() {ec()};
+function ec(){
+    eu.currentTime = 0;
+    eu.play();
+}
+var fu = document.getElementById("faud");
+var fq=document.getElementById("f");
+fq.onmousedown=function() {fc()};
+function fc(){
+    fu.currentTime = 0;
+    fu.play();
+}
+var gu = document.getElementById("gaud");
+var gq=document.getElementById("g");
+gq.onmousedown=function() {gc()};
+function gc(){
+    gu.currentTime = 0;
+    gu.play();
+}
